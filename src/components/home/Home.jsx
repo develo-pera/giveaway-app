b928469c303b9e27a238bc7f74fb5400aae84863
Petar Popovic <petarpnp@gmail.com>
2019-02-08 16:00:20 +0100

    Create Home component Layout

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Intro from '../intro/Intro'
+
+import styles from './Home.module.scss'
+
+const Home = () => (
+  <div className={styles.wrapper}>
+    <Intro />
+  </div>
+)
+
+export default Home
