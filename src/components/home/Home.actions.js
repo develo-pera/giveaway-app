bb6f3c7e0e9333e11c43a647da5714b7aa9b9c06
Petar Popovic <petarpnp@gmail.com>
2019-02-09 20:28:36 +0100

    Create action for going to the next page

---
+++ src/components/home/Home.actions.js
@@ -0,0 +1,11 @@
+import ACTION_TYPES from './Home.actionTypes'
+
+const dispatchIncrementCurrentPage = () => ({
+  type: ACTION_TYPES.INCREMENT_CURRENT_PAGE_NUMBER,
+})
+
+const goToNextStep = () => {
+  dispatchIncrementCurrentPage()
+}
+
+export default goToNextStep
