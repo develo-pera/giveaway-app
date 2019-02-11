071eae991128294bf4b286ea5818997bdc99c0bf
Petar Popovic <petarpnp@gmail.com>
2019-02-11 14:20:20 +0100

    Implement increment and decrement logic in InputNumber component

--- src/components/common/input-number/InputNumber.jsx
+++ src/components/common/input-number/InputNumber.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import styles from './InputNumber.module.scss'
 
-const InputNumber = () => (
+const InputNumber = ({ value, onIncrement, onDecrement }) => (
   <div className={styles.input}>
-    <div className={styles.decrementButton}>
+    <div
+      className={styles.decrementButton}
+      onClick={onDecrement}
+    >
       <FontAwesomeIcon icon={faMinus} />
     </div>
-    <div className={styles.numberInput}>0</div>
-    <div className={styles.incrementButton}>
+    <div className={styles.numberInput}>
+      {value}
+    </div>
+    <div
+      className={styles.incrementButton}
+      onClick={onIncrement}
+    >
       <FontAwesomeIcon icon={faPlus} />
     </div>
   </div>
 )
+
+InputNumber.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired,
+}
 
 export default InputNumber
