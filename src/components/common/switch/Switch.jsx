cd08111fffb8dd631b8b345840f3352c7e2aa827
Petar Popovic <petarpnp@gmail.com>
2019-05-26 01:50:34 +0200

    Make common Switch component dynamic

--- src/components/common/switch/Switch.jsx
+++ src/components/common/switch/Switch.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,24 +7,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './Switch.module.scss'
 
 class Switch extends Component {
-  state = {
-    on: false,
+  onSwitchClick = () => {
+    const { name, onClick } = this.props
+    if (onClick && name) {
+      onClick(name)
+    } else {
+      throw new Error('Please add required attributes onClick and name');
+    }
   }
 
-  toggleSwitchState = () => this.setState(prevState => ({
-    on: !prevState.on,
-  }))
-
   render() {
-    const { on } = this.state
+    const { value } = this.props
 
     return (
       <div
-        className={`${styles.switch} ${on ? styles.switchOn : styles.switchOff}`}
-        onClick={this.toggleSwitchState}
+        className={`${styles.switch} ${value ? styles.switchOn : styles.switchOff}`}
+        onClick={this.onSwitchClick}
       >
         {
-          on ?
+          value ?
             (
               <FontAwesomeIcon
                 className={styles.icon}
@@ -40,6 +42,16 @@ class Switch extends Component {
       </div>
     )
   }
+}
+
+Switch.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+}
+
+Switch.defaultProps = {
+  value: false,
 }
 
 export default Switch
