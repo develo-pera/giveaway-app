b1d6e5a017857fbb73f72bd92006acb2e2919be7
Petar Popovic <petarpnp@gmail.com>
2020-03-07 12:29:43 +0100

    Change select profile to input

--- src/components/select-profile/select-profile.jsx
+++ src/components/select-profile/select-profile.jsx
@@ -1,139 +1,35 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-import goToNextStep from '../picker/Picker.actions'
-import getUserInstagramProfiles from './select-profile.actions'
-import { getInstagramPhotosForProfileWithId } from '../select-post/SelectPost.actions'
-import Loader from '../common/loader/Loader'
-import Emoji from '../common/emoji/Emoji'
-import DefaultPhoto from '../../assets/images/default-profile-photo.jpg'
+import goToNextStep from '../picker/Picker.actions';
 
-import styles from './select-profile.module.scss'
+import styles from './select-profile.module.scss';
+import Button from '../common/button/Button';
 
-class SelectProfile extends React.Component {
-  componentDidMount() {
-    const { boundGetUserInstagramProfiles } = this.props
-    boundGetUserInstagramProfiles();
-  }
-
-  renderErrorMessage = () => (
-    <div className={styles.noPostsMessageWrapper}>
-      <Emoji
-        symbol=" 🤷🏻‍♂️"
+const SelectProfile = ({ boundGoToNextStep }) => (
+  <div className={styles.container}>
+    <p className={styles.title}>
+      Please enter Instagram username of the profile you want to run contest on:
+    </p>
+    <div className={styles.form}>
+      <input className={styles.input} type="text" />
+      <Button
+        type="button"
+        label="Start"
+        btnClass={styles.button}
+        onClick={boundGoToNextStep}
       />
-      <p className={`${styles.text} ${styles.error}`}>
-        Unexpected error occurred and we can't fetch your instagram accounts, please try later
-      </p>
     </div>
-  )
-
-  renderNoProfilesMessage = () => (
-    <div className={styles.noPostsMessageWrapper}>
-      <Emoji
-        symbol="🙈"
-      />
-      <p className={styles.text}>
-        Looks like you have no instagram accounts associated with your facebook profile
-      </p>
-    </div>
-  )
-
-  render() {
-    const {
-      isLoading,
-      instagramProfiles,
-      isError,
-      boundGoToNextStep,
-      boundGetInstagramPhotosForProfileWithId,
-    } = this.props
-
-    return (
-      <div className={styles.container}>
-        {
-          isLoading &&
-          <Loader />
-        }
-        {
-          isError &&
-          this.renderErrorMessage()
-        }
-        {
-          !isLoading &&
-          !isError &&
-          instagramProfiles.length === 0 &&
-          this.renderNoProfilesMessage()
-        }
-        {
-          !isLoading &&
-          !isError &&
-          instagramProfiles.length > 0 &&
-          (
-            <React.Fragment>
-              <p className={styles.title}>Select profile:</p>
-              <div className={styles.profilesGrid}>
-                {
-                  instagramProfiles.map(profile => (
-                    <div
-                      onClick={() => {
-                        boundGetInstagramPhotosForProfileWithId(profile.id)
-                        boundGoToNextStep()
-                      }}
-                      className={styles.card}
-                    >
-                      <img
-                        className={styles.profileImage}
-                        src={
-                          profile.profile_picture_url ||
-                          DefaultPhoto
-                        }
-                        alt={profile.username}
-                      />
-                      <div>
-                        <p className={styles.text}>{ `@${profile.username}` }</p>
-                        <p className={styles.text}>{ profile.name }</p>
-                        <p className={styles.text}>
-                          Followers:
-                          {' '}
-                          { profile.followers_count }
-                        </p>
-                        <p className={styles.text}>
-                          Follows:
-                          {' '}
-                          { profile.follows_count }
-                        </p>
-                      </div>
-                    </div>
-                  ))
-                }
-              </div>
-            </React.Fragment>
-          )
-        }
-      </div>
-    )
-  }
-}
+  </div>
+);
 
 SelectProfile.propTypes = {
-  isLoading: PropTypes.bool.isRequired,
-  instagramProfiles: PropTypes.arrayOf().isRequired,
-  isError: PropTypes.bool.isRequired,
   boundGoToNextStep: PropTypes.func.isRequired,
-  boundGetUserInstagramProfiles: PropTypes.func.isRequired,
-  boundGetInstagramPhotosForProfileWithId: PropTypes.func.isRequired,
-}
-
-const mapStateToProps = state => ({
-  isLoading: state.profilesReducer.isLoading,
-  instagramProfiles: state.profilesReducer.profiles,
-  isError: state.profilesReducer.isError,
-})
+};
 
 const mapDispatchToProps = {
   boundGoToNextStep: goToNextStep,
-  boundGetUserInstagramProfiles: getUserInstagramProfiles,
-  boundGetInstagramPhotosForProfileWithId: getInstagramPhotosForProfileWithId,
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(SelectProfile)
+export default connect(null, mapDispatchToProps)(SelectProfile);
